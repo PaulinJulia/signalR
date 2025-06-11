47502a9b2a2615fd56f1cfa6aa62a530d002fad4
julia.paulin <julia.paulin@chasacademy.se>
2025-06-11 22:23:12 +0200

    Add default file serving and remove unused weather forecast endpoint

--- SignalR-server/Program.cs
+++ SignalR-server/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors();
@@ -44,33 +45,7 @@ if (!app.Environment.IsProduction())
     app.UseHttpsRedirection();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-
-app.MapGet("/", () => "SignalR backend is running 🎉");
 app.MapFallbackToFile("index.html");
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
