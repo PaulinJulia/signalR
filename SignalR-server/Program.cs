84b24736fd6b5da7afa9c45381d26983b6634d70
julia.paulin <julia.paulin@chasacademy.se>
2025-06-11 22:11:05 +0200

    Update CORS policy to include chatHub origin for SignalR connections

--- SignalR-server/Program.cs
+++ SignalR-server/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:5263", "https://signalrchat-kgrw.onrender.com")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:5263", "https://signalrchat-kgrw.onrender.com", "https://signalrchat-kgrw.onrender.com/chatHub")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
